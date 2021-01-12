279cf6044ffb445274b369bc9575efac49f8bf47
hzy1001 <hzy1001@moumou.co.kr>
2021-01-12 18:03:05 +0900

    노드http에서 pwa 및 jquery 사용시 정적페이지로 라우팅해야함.

---
+++ game.server.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var app = express(); 
+var http = require('http'); 
+
+var server = http.Server(app);       //익스프레스를 탑제한 http서버
+var socket = require('socket.io');   //소켓 IO
+var io = socket(server);             //웹서버를 탑제한 소켓 IO
+var i = 0;
+
+// app.use('/', function(req, resp) {   //익스프레스 라우팅
+//     resp.sendFile(__dirname + '/index.html');
+// });  
+
+//정적 페이지로 연결되도록 한다.
+app.use(express.static(__dirname + '/'));
+
+
+server.listen(8088, function() {
+    console.log('Server On !');
+});
+
+io.on('connection', function(socket) {  
+      
+    //i = i + 1;
+
+    socket.on('SEND', function(msg) {
+        console.log(msg); 
+        i= msg;
+    }); 
+
+        
+    socket.emit('getusername', "홍길동" + i); 
+
+
+    // socket.on('disconnect', function() { 
+    //     console.log("사용자 연결 해제");
+    // }); 
+
+});
+
+// function Player(id)
+// {
+//     this.id = id;
+//     this.isWantGame = false;
+//     this.rival_id = 0;
+// }
+
+
+// import.sockets.on("connection",function(socket)
+// {
+//     var player;
+//     socket.get("user_data", function(error, user_data)
+//     {
+//         player = user_data;
+//     });
+
+//     player.isWantGame = true;
+
+//     for(var i = 0; i < arrPlayers.length; i++)
+//     {
+//         if(arrPlayers[i].id == player.id)
+//         {
+//             continue;
+
+//             if(arrPlayers[i].isWantGame == true)
+//             {
+//                     console.log("[start game]");
+//                     player.isWantGame = false;
+//                     arrPlayers[i].isWantGame = false;
+
+//                     player.rival_id = arrPlayers[i].id;
+//                     arrPlayers[i].isWantGame = false;
+
+//                     io.sockets.sockets[player.id].emit("start_game");
+//                     io.sockets.sockets[arrPlayers[i].id].emit("start_gmae");
+//             } 
+//         }
+//     } 
+// });
